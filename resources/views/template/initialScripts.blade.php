4361eaf85becde68b0a126a1719780ddadc856a7
Daespinosag <daespinosag@unal.edu.co>
2017-07-03 22:04:09 -0500

    configure vue.js

--- resources/views/template/initialScripts.blade.php
+++ resources/views/template/initialScripts.blade.php
@@ -7,6 +7,12 @@
     <meta name="description" content="Short explanation about this website">
     <!-- END META -->
 
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+
     <!-- BEGIN STYLESHEETS -->
     <link href='http://fonts.googleapis.com/css?family=Roboto:300italic,400italic,300,400,500,700,900' rel='stylesheet' type='text/css'/>
     <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/theme-default/bootstrap.css') }}" />
@@ -15,7 +21,9 @@
     <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/theme-default/material-design-iconic-font.min.css') }}" />
     <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/theme-default/libs/rickshaw/rickshaw.css') }}" />
     <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/theme-default/libs/morris/morris.core.css') }}" />
-    <script src="https://unpkg.com/vue"></script>
+
+
+
     <!-- END STYLESHEETS -->
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
