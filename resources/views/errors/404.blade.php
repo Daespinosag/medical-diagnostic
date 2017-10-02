7fbc417c839624d9168dfcd27b19165b373c281a
Daespinosag <daespinosag@unal.edu.co>
2017-10-02 17:11:19 -0500

    view errors

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('template.main')
+
+@section('content')
+
+    <div class="col-lg-12 text-center">
+        <h1><span class="text-xxxl text-light">404 <i class="fa fa-search-minus text-primary"></i></span></h1>
+        <h2 class="text-light">Pagina no encontrada</h2>
+    </div>
+@endsection
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,8 @@
+@extends('template.main')
+
+@section('content')
+    <div class="col-lg-12 text-center">
+        <h1><span class="text-xxxl text-light">500 <i class="fa fa-exclamation-circle text-danger"></i></span></h1>
+        <h2 class="text-light">Algo ha salido mal</h2>
+    </div>
+@endsection
